20ab9fbedc5766f224250521e65c4a770af3c0d5
Julian Alvaran <jalvaran@gmail.com>
2020-05-14 16:08:31 -0500

    SE dibujan los productos, falta paginar

--- modelo/php_settings.php
+++ modelo/php_settings.php
@@ -3,6 +3,6 @@
  * Aqui se establecen los datos de conexion a la base de datos
  */
 const RECAPTCHA_V3_SECRET_KEY="6LdoC-gUAAAAALqCKzE55QKBmHrMkRsmbq09wq2c";
-const HOST="35.226.201.57", USER="root",PW="pirlo1985",DB="ts6_domi";
+const HOST="localhost", USER="root",PW="pirlo1985",DB="ts6_domi";
 
 ?>
